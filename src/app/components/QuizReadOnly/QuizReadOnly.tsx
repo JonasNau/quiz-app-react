0dd896c2dc67981822660af687b415a01f1d9885
jnaumann <jonas.naumann@ibykus.de>
2024-03-06 13:08:49 +0100

    Hinzufügen der Funktion für die Größenanpassung von Bildern

--- src/app/components/QuizReadOnly/QuizReadOnly.tsx
+++ src/app/components/QuizReadOnly/QuizReadOnly.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./quizReadOnly.module.scss";
 import { QuestionEntry } from "@/interfaces/joi/QuizSchemas";
 import Answer from "../Answer/Answer";
 import { Image } from "react-bootstrap";
+import { Resizable } from "re-resizable";
 
 export default function QuizReadOnly({
 	questionEntry,
@@ -12,29 +12,51 @@ export default function QuizReadOnly({
 	questionEntry: QuestionEntry;
 	showSolutions: boolean;
 }) {
+	const [resizableWidth, setResizableWidth] = useState("100%");
+
+	const handleResetSize = () => {
+		setResizableWidth("100%");
+	};
+
 	return (
 		<div className={styles.quizReadOnly}>
 			<h2 className="text-center">Frage</h2>
 			<div className="text-center question">{questionEntry.question}</div>
 
 			{questionEntry.image && (
-				<div className="image-wrapper">
-					<Image
-						className={`image image-box-shadow`}
-						src={questionEntry.image.base64}
-						alt="Bild für die Fragestellung"
-					/>
+				<div className="image-wrapper" onDoubleClick={() => handleResetSize()}>
+					<Resizable
+						size={{ width: resizableWidth, height: "auto" }}
+						onResizeStop={(e, direction, ref, d) => {
+							setResizableWidth(ref.style.width);
+						}}
+						maxWidth={"100%"}
+						resizeRatio={1}
+						lockAspectRatio={true}
+					>
+						<a
+							onClick={() => {
+								console.log(questionEntry.image);
+							}}
+						>
+							<Image
+								className={`image image-box-shadow`}
+								src={questionEntry.image.base64}
+								alt="Bild für die Fragestellung"
+							/>
+						</a>
+					</Resizable>
 				</div>
 			)}
 
 			<section className="answersList">
-				{questionEntry.answers.map((anwser) => {
+				{questionEntry.answers.map((answer) => {
 					if (showSolutions) {
 						return (
-							<Answer text={anwser.text} isCorrect={anwser.isCorrect} key={anwser.text} />
+							<Answer text={answer.text} isCorrect={answer.isCorrect} key={answer.text} />
 						);
 					}
-					return <Answer text={anwser.text} key={anwser.text} />;
+					return <Answer text={answer.text} key={answer.text} />;
 				})}
 			</section>
 		</div>
